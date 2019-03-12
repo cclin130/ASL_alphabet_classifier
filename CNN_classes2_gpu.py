050f1e0558c0e1c1f66340eeef95e861e256ee94
cclin130 <cheng.lin103@gmail.com>
2019-03-12 11:02:16 -0400

    remove one CNN layer

--- CNN_classes2_gpu.py
+++ CNN_classes2_gpu.py
@@ -47,13 +47,13 @@ class CNN(nn.Module):
     def __init__(self, output_dim):
         super().__init__()
         
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=0)
-        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(3, 1, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=0)
+        #self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=0)
         
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        self.fc1 = nn.Linear(16928, 900)
+        self.fc1 = nn.Linear(64*48*48, 900)
         self.fc2 = nn.Linear(900, output_dim)
         
     def forward(self, x):
@@ -76,15 +76,15 @@ class CNN(nn.Module):
         #print(x.shape)
         
         #print('conv 3')
-        x=F.relu(self.conv3(x))
+        #x=F.relu(self.conv3(x))
         #print(x.shape)
-        x = self.pool(x)
+        #x = self.pool(x)
         #print('pool')
         #print(x.shape)
         
         #reshape data for fully connected layer
         #print('view')
-        x = x.view(-1, 16928)
+        x = x.view(-1, 64*48*48)
         #print(x.shape)
         
         #fully connected layers
