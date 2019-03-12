6fc7cfa6f049f87684afd47269a5432d060f5c7d
cclin130 <cheng.lin103@gmail.com>
2019-03-12 14:42:42 -0400

    LeNet architecture

--- CNN_classes2_gpu.py
+++ CNN_classes2_gpu.py
@@ -18,10 +18,10 @@ device = torch.device('cuda')
 
 #function to read in image and normalize it
 def get_normalized_image(image_path):
-    img = cv.imread(image_path, True)
-    img = np.swapaxes(img, 0,2)
+    img = cv.imread(image_path, 0)
+    #img = np.swapaxes(img, 0,2)
     img_norm = (img-np.mean(img))/np.std(img)
-    tensor_img_norm = torch.from_numpy(img_norm)
+    tensor_img_norm = torch.from_numpy(img_norm).unsqueeze(0)
     
     return tensor_img_norm
 
@@ -47,52 +47,53 @@ class CNN(nn.Module):
     def __init__(self, output_dim):
         super().__init__()
         
-        self.conv1 = nn.Conv2d(3, 1, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=0)
-        #self.conv3 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=0)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=0)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0)
+        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv5 = nn.Conv2d(16, 120, kernel_size=5, stride=1, padding=0)
         
-        self.fc1 = nn.Linear(32*48*48, 500)
-        self.fc2 = nn.Linear(500, output_dim)
+        self.fc6 = nn.Linear(120*43*43, 84)
+        self.fc7 = nn.Linear(84, output_dim)
         
     def forward(self, x):
         #cast to float
         x = x.float()
         
         #convolutional layer + pooling
-        #print('conv 1')
+        print('conv 1')
         x = F.relu(self.conv1(x))
-        #print(x.shape)
-        #print('pool')
-        x = self.pool(x)
-        #print(x.shape)
+        print(x.shape)
+        print('pool')
+        x = self.pool2(x)
+        print(x.shape)
         
-        #print('conv 2')
-        x=F.relu(self.conv2(x))
-        #print(x.shape)
-        x = self.pool(x)
-        #print('pool')
-        #print(x.shape)
+        print('conv 3')
+        x=F.relu(self.conv3(x))
+        print(x.shape)
+        x = self.pool4(x)
+        print('pool')
+        print(x.shape)
         
-        #print('conv 3')
-        #x=F.relu(self.conv3(x))
-        #print(x.shape)
+        print('conv 5')
+        x=F.relu(self.conv5(x))
+        print(x.shape)
         #x = self.pool(x)
         #print('pool')
         #print(x.shape)
         
         #reshape data for fully connected layer
-        #print('view')
-        x = x.view(-1, 32*48*48)
-        #print(x.shape)
+        print('view')
+        x = x.view(-1, 120*43*43)
+        print(x.shape)
         
         #fully connected layers
-        #print('linear')
-        x = F.relu(self.fc1(x))
-        #print(x.shape)
-        x = F.relu(self.fc2(x))
-        #print(x.shape)
+        print('linear')
+        x = F.relu(self.fc6(x))
+        print(x.shape)
+        x = F.relu(self.fc7(x))
+        print(x.shape)
         
         return x.squeeze()
         
