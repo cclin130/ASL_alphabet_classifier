00829e52e40438a4c870565f5d48e3a026f84850
Cheng Lin <cheng.lin103@gmail.com>
2019-03-27 01:06:30 -0400

    add feature to take photo with space bar

--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,13 @@ if (navigator.mediaDevices.getUserMedia) {
     });
 }
 
+//run snap when spacebar is pressed
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        snap()
+    }
+}
+
 //deifne a function for when button is pressed
 function snap()
 {
@@ -25,31 +32,31 @@ function snap()
 
             //take the photo of the single screen capture
             imageCapture.takePhoto()
-              .then(blob => {
-                //once photo is taken, create a formData object to store it
-                video.src = URL.createObjectURL(blob);
-                video.onload = () => { URL.revokeObjectURL(this.src); }
+                .then(blob => {
+                    //once photo is taken, create a formData object to store it
+                    video.src = URL.createObjectURL(blob);
+                    video.onload = () => { URL.revokeObjectURL(this.src); }
 
-                var formData = new FormData();
-                formData.append("image", blob);
+                    var formData = new FormData();
+                    formData.append("image", blob);
 
-                //send an XMLHttpRequest to flask backend
-                var request = new XMLHttpRequest();
-                request.open("POST", "http://localhost:5000/");
-                request.withCredentials = true;
+                    //send an XMLHttpRequest to flask backend
+                    var request = new XMLHttpRequest();
+                    request.open("POST", "http://localhost:5000/");
+                    request.withCredentials = true;
 
-                //define an onload function for when backend returns response
-                request.onload = function (){
-                    if (request.readyState === request.DONE){
-                        if (request.status === 200){
-                            console.log(request.response);
-                            var result = document.getElementById("output");
-                            var result_display = document.getElementById("output_display");
-                            result.innerHTML = request.response;
-                            result_display.style.visibility = "visible";
-                        }
-                    }                
-                }
+                    //define an onload function for when backend returns response
+                    request.onload = function (){
+                        if (request.readyState === request.DONE){
+                            if (request.status === 200){
+                                console.log(request.response);
+                                var result = document.getElementById("output");
+                                var result_display = document.getElementById("output_display");
+                                result.innerHTML = request.response;
+                                result_display.style.visibility = "visible";
+                            }
+                        }                
+                    }
 
                 request.send(formData);
 
