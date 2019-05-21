9808682d2d2702f73310f3a0ce472d32bf98d957
cclin130 <clin@DESKTOP-S4DUQCQ.localdomain>
2019-05-20 22:11:37 -0400

    decrease app memory requirement

--- web_app.py
+++ web_app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, make_response
 from functools import wraps, update_wrapper
 from PIL import Image
@@ -19,5 +20,7 @@ def upload():
 	else:
 		return render_template('home.html', prediction=None)
 
+
 if __name__ == '__main__':
-   app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
